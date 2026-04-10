629f95cf0e808140aac8821d14a28fb16486ca54
Averydimbulb79 <70785439+Averydimbulb79@users.noreply.github.com>
2026-04-10 18:48:19 +0800

    Create mme-pwa-sw.js

---
+++ mme-pwa-sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "mme-pwa-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./Mike_Markdown_Editor_v7_6_pwa.html"
+];
+
+// Install
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
